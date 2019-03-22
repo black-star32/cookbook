8427dbabd599093f35c2c10e45df210582980dcd
Bo Feng <453431821@qq.com>
2019-03-22 17:05:14 +0800

    20190322

--- 1.18.2.py
+++ 1.18.2.py
@@ -62,3 +62,10 @@ stock_prototype = Stock('', 0, 0.0, None, None)
 # Function to convert a dictionary to a Stock
 def dict_to_stock(s):
     return stock_prototype._replace(**s)
+
+a = {'name': 'ACME', 'shares': 100, 'price': 123.45}
+print(dict_to_stock(a))
+
+
+b = {'name': 'ACME', 'shares': 100, 'price': 123.45, 'date': '12/17/2012'}
+print(dict_to_stock(b))
