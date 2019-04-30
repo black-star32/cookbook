97e95782b27de23755d92a2305e29f133c8d4613
Bo Feng <453431821@qq.com>
2019-04-30 10:09:41 +0800

    20190430

--- 2/2.15.2.py
+++ 2/2.15.2.py
@@ -16,5 +16,27 @@ class Info:
 a = Info('Guido', 37)
 print(s.format_map(vars(a)))
 
-# format 和 format_map() 的一个缺陷就是它们并不能很好的处理变量缺失的情况
-print(s.format_map(name='Guido'))
+# # format 和 format_map() 的一个缺陷就是它们并不能很好的处理变量缺失的情况
+# # print(s.format_map(name='Guido'))
+
+# 一种避免这种错误的方法是另外定义一个含有 __missing__() 方法的字典对象，就像下面这样
+class safesub(dict):
+    """防止key找不到"""
+    def __missing__(self, key):
+        return '{' + key + '}'
+
+# 现在你可以利用这个类包装输入后传递给 format_map() ：
+name = 'Guido'
+print(s.format_map(safesub(vars())))
+
+# 如果你发现自己在代码中频繁的执行这些步骤，你可以将变量替换步骤用一个工具函数封装起来。就像下面这样：
+import sys
+
+def sub(text):
+    return text.format_map(safesub(sys._getframe(1).f_locals))
+
+name = 'Guido'
+n = 37
+print(sub('Hello {name}'))
+print(sub('You have {n} messages.'))
+print(sub('Your favorite color is {color}'))
